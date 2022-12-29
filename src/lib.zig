ff31b3eacb756e084c01982109be841c381b3855
Morgan Rosenkranz <rosenkranz1999@gmail.com>
2022-12-29 18:24:06 -0500

    moved tests for parser to new file

--- src/lib.zig
+++ src/lib.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 pub const chunk = @import("chunk.zig");
 pub const inst = @import("inst.zig");
 pub const scanner = @import("scanner.zig");
+pub const parser = @import("parser.zig");
 pub const compiler = @import("compiler.zig");
 pub const vm = @import("vm.zig");
 pub const ast = @import("ast.zig");
